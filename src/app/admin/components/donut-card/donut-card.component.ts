a798766aae3f7c51efa83f1d264e1de65ea03b15
Kristof Kox <kristof.kox@ravago.com>
2023-10-26 21:07:40 +0200

    Scoped Styles in Components

--- src/app/admin/components/donut-card/donut-card.component.ts
+++ src/app/admin/components/donut-card/donut-card.component.ts
@@ -10,17 +10,44 @@ import {Donut} from "../../models/donut.model";
         [alt]="donut.name"
         class="donut-card-icon"
       />
+      <div>
+        <p class="donut-card-name">
+          {{ donut.name }}
+        </p>
+        <p class="donut-card-price">
+          {{ donut.price }}
+        </p>
+      </div>
     </div>
-    <div>
-      <p class="dunut-card-name">
-        {{ donut.name }}
-      </p>
-      <p class="dunut-card-price">
-        {{ donut.price }}
-      </p>
-    </div>
+
   `,
   styles: [
+    `
+      .donut-card { // verwijst naar <div class="donut-card">
+        display: flex;
+        align-items: center;
+        background: #f7f7f7;
+        border-radius: 5px;
+        margin-bottom: 5px;
+        padding: 5px 15px;
+        transition: transform 0.2s ease-in-out;
+        &:hover {
+          transform: translateY(-3px);
+        }
+        &-name {  // nieuw in "style": "scss" Hierdoor hoef je niet de volledige class op te geven en verwijst naar <p class="donut-card-name">
+                  // "scss" wordt uitgesproken als sass
+          font-size: 16px;
+        }
+        &-price { // verwijst naar <p class="donut-card-price">
+          font-size: 14px;
+          color: #c14583;
+        }
+        &-icon { // verwijst naar <p class="donut-card-icon">
+          width: 50px;
+          margin-right: 10px;
+        }
+      }
+    `,
   ]
 })
 export class DonutCardComponent {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,15 +5,30 @@ import * as events from "events";
   selector: 'app-root',
   template: `
     <div class="app">
+      <header class="header">
+        <img src="/assets/img/logo.svg" alt="Ultimate Donuts" class="logo">
+      </header>
       <donut-list></donut-list>
     </div> `,
   styles: [
     `
       .app {
-        margin-top: 50px;
-        font-size: 22px;
-        color: #fff;
-        text-align: center;
+        background: #fff;
+        border-radius: 8px;
+        max-width: 400px;
+        width: 94%;
+        margin: 25px auto;
+        padding: 25px;
+        border: 4px solid #ef9fc7;
+      }
+      .header {
+        display: flex;
+        justify-content: center;
+        margin-bottom: 25px;
+      }
+      .logo {
+        width: 100px;
+        height: 88px;
       }
     `,
   ],
