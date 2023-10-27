3f0b10a22b1f3ead06c350a004b39ae403881437
Kristof Kox <kristof.kox@ravago.com>
2023-10-27 06:14:31 +0200

    Complex Inline Styles with NgStyle

--- src/app/admin/components/donut-card/donut-card.component.ts
+++ src/app/admin/components/donut-card/donut-card.component.ts
@@ -5,8 +5,14 @@ import {Donut} from "../../models/donut.model";
   selector: 'donut-card',
   encapsulation: ViewEncapsulation.Emulated, // Is de default dat wil zeggen dat nu de css van styles.scss genomen wordt
   template: `
-    <div class="donut-card"
-          [style.border]="donut.promo ? '2px solid #eee': 'none' ">
+      <div class="donut-card"
+           [ngStyle]=" {
+                border: donut.promo ? '2px solid #eee': 'none',
+                'background-color': donut.promo ? 'red': 'green',
+                'font-size.px': 26
+          }"
+           >
+        <!-- commented out in styles: [...-->
       <img
         src="/assets/img/{{ donut.icon }}.svg"
         [alt]="donut.name"
@@ -36,15 +42,14 @@ import {Donut} from "../../models/donut.model";
         &:hover {
           transform: translateY(-3px);
         }
-        &-name {  // nieuw in "style": "scss" Hierdoor hoef je niet de volledige class op te geven en verwijst naar <p class="donut-card-name">
-                  // "scss" wordt uitgesproken als sass
-          font-size: 16px;
+        &-name {
+          //font-size: 16px;
         }
-        &-price { // verwijst naar <p class="donut-card-price">
-          font-size: 14px;
+        &-price {
+          //font-size: 14px;
           color: #c14583;
         }
-        &-icon { // verwijst naar <p class="donut-card-icon">
+        &-icon {
           width: 50px;
           margin-right: 10px;
         }
