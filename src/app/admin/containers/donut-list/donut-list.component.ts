9562c7b5dc4bb18a64d93c4fc040c680d11c4809
Kristof Kox <kristof.kox@ravago.com>
2023-10-30 22:21:16 +0100

    NgIf Syntax with <ng-template> vs <ng-container>

--- src/app/admin/containers/donut-list/donut-list.component.ts
+++ src/app/admin/containers/donut-list/donut-list.component.ts
@@ -5,19 +5,34 @@ import { Donut } from "../../models/donut.model";
 @Component({
   selector: 'donut-list',
   template: `
-      <div id="krko">
-          <ng-container *ngIf="donuts.length; then cards; else nothing">
-          </ng-container>
-      </div>
+        // ng-container vs ng-template:
+        // Een ng-container wordt altijd gerendered
+        // terwijl een ng-template pas gerenderd wordt op basis van een voorwaarde
+        // Volgende zijn gelijk
+        <div>
+            <ng-container *ngIf="donuts.length; else nothing">
+                <donut-card [donut]="donuts[0]"></donut-card>
+                <donut-card [donut]="donuts[1]"></donut-card>
+                <donut-card [donut]="donuts[2]"></donut-card>
+            </ng-container>
 
-      <ng-template #cards>
-          <donut-card [donut]="donuts[0]"></donut-card>
-          <donut-card [donut]="donuts[1]"></donut-card>
-          <donut-card [donut]="donuts[2]"></donut-card>
-      </ng-template>
-      <ng-template #nothing>
-          <p>No Donust here..</p>
-      </ng-template>
+            <ng-template [ngIf]="donuts.length" [ngIfElse]="nothing">
+                <donut-card [donut]="donuts[0]"></donut-card>
+                <donut-card [donut]="donuts[1]"></donut-card>
+                <donut-card [donut]="donuts[2]"></donut-card>
+            </ng-template>
+
+          // Volgende wordt niet gerenderd
+            <ng-template>
+                <donut-card [donut]="donuts[0]"></donut-card>
+                <donut-card [donut]="donuts[1]"></donut-card>
+                <donut-card [donut]="donuts[2]"></donut-card>
+            </ng-template>
+
+            <ng-template #nothing>
+                <p>No Donust here..</p>
+            </ng-template>
+        </div>
   `,
   styles: [
   ]
@@ -28,28 +43,28 @@ export class DonutListComponent implements OnInit {
 
   ngOnInit(): void {
     this.donuts = [
-      // {
-      //   id: 'xy12yr',
-      //   name: 'Just Chocolate',
-      //   icon: 'just-chocolate',
-      //   price: 119,
-      //   description: 'For the pure chocoholic.',
-      // },
-      // {
-      //   id: 'zy19yr',
-      //   name: 'Glazed Fudge',
-      //   icon: 'glazed-fudge',
-      //   price: 129,
-      //   promo: true,
-      //   description: 'Sticky perfection.',
-      // },
-      // {
-      //   id: 'qy19ya',
-      //   name: 'Caramel Swirl',
-      //   icon: 'caramel-swirl',
-      //   price: 129,
-      //   description: 'Chocolate drizzled with caramel',
-      // }
+      {
+        id: 'xy12yr',
+        name: 'Just Chocolate',
+        icon: 'just-chocolate',
+        price: 119,
+        description: 'For the pure chocoholic.',
+      },
+      {
+        id: 'zy19yr',
+        name: 'Glazed Fudge',
+        icon: 'glazed-fudge',
+        price: 129,
+        promo: true,
+        description: 'Sticky perfection.',
+      },
+      {
+        id: 'qy19ya',
+        name: 'Caramel Swirl',
+        icon: 'caramel-swirl',
+        price: 129,
+        description: 'Chocolate drizzled with caramel',
+      }
     ]
   }
 }
