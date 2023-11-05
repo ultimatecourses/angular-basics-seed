c8733e2bc4edb0e5a523ce5f894694cfbd71b1ae
Kristof Kox <kristof.kox@ravago.com>
2023-11-05 18:46:15 +0100

    Advanced NgSwitch Syntax with <ng-template>

--- src/app/admin/components/donut-card/donut-card.component.ts
+++ src/app/admin/components/donut-card/donut-card.component.ts
@@ -20,11 +20,10 @@ import {Donut} from "../../models/donut.model";
         <p class="donut-card-name">
           {{ donut.name }}
             <ng-container [ngSwitch]="donut.promo">
-                <span *ngSwitchCase=" 'new' " class="donut-card-label"
-                style="color: red; border-color: red">NEW</span>
-                <span *ngSwitchCase=" 'limited' " class="donut-card-label">LIMITED</span>
-                <span *ngSwitchDefault class="donut-card-label">
-                  Nothing special...
+                <span class="donut-card-label">
+                    <ng-template [ngSwitchCase]="'new'"> NEW </ng-template>
+                    <ng-template [ngSwitchCase]="'limited'"> LIMITED </ng-template>
+                    <ng-template ngSwitchDefault> Nothing special... </ng-template>
                 </span>
             </ng-container>
         </p>
