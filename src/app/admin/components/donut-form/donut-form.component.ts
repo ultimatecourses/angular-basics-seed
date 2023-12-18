7418226a41f904a68a873094d2dbc0e39e96483c
Kristof Kox <kristof.kox@ravago.com>
2023-12-18 15:49:36 +0100

    Radio Button Binding with NgModel

--- src/app/admin/components/donut-form/donut-form.component.ts
+++ src/app/admin/components/donut-form/donut-form.component.ts
@@ -7,13 +7,53 @@ import {Component, OnInit} from '@angular/core';
       <label>
         <span>Name</span>
         <input type="text" name="name" class="input" ngModel/>
+      </label>
+
+      <label>
         <span>Price</span>
         <input type="number" name="price" class="input" ngModel/>
       </label>
-     <pre> {{ form.value | json}} </pre>
+
+      <div class="donut-form-radios">
+        <p class="donut-form-radios-label">Promo:</p>
+        <label>
+<!--          [value] >> property binding. Met undefined kan je de promo weg laten in de json  -->
+          <input type="radio" name="promo" [value]="undefined" ngModel/>
+          <span>None</span>
+        </label>
+        <label>
+          <input type="radio" name="promo" value="new" ngModel/>
+          <span>New</span>
+        </label>
+        <label>
+          <input type="radio" name="promo" value="limited" ngModel/>
+          <span>Limited</span>
+        </label>
+      </div>
+
+     <pre> {{ form.value | json }} </pre>
     </form>
   `,
   styles: [
+    `
+      .donut-form {
+      &-radios {
+        display: flex;
+        align-content: center;
+        &-label {
+          margin-right: 10px;
+        }
+        label {
+          display: flex;
+          align-items: center;
+          span {
+            color: #444;
+            margin-bottom: 0;
+          }
+        }
+      }
+    }
+  `
   ]
 })
 export class DonutFormComponent implements OnInit {
