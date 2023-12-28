e2e56790dd6da7bcaee7c8b3fb5f1c4df14631b4
Kristof Kox <kristof.kox@ravago.com>
2023-12-28 12:05:23 +0100

    Root Module Routes with forRoot()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import {Component, OnInit} from '@angular/core';
       <header class="header">
         <img src="/assets/img/logo.svg" alt="Ultimate Donuts" class="logo">
       </header>
-      <donut-single></donut-single>
+      <router-outlet></router-outlet>
     </div> `,
   styles: [
     `
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {RouterModule, Routes} from "@angular/router";
 
 import { AppComponent } from './app.component';
 import { AdminModule } from './admin/admin.module';
+import {DonutListComponent} from "./admin/containers/donut-list/donut-list.component";
+import {DonutSingleComponent} from "./admin/containers/donut-single/donut-single.component";
 
+// path: '' is http://localhost:4200/
+export const routes: Routes = [
+  { path: '', component: DonutListComponent },
+  { path: 'donut', component: DonutSingleComponent },
+];
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AdminModule],
+  imports: [BrowserModule, RouterModule.forRoot(routes), AdminModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
